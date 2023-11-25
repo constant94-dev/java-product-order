1e62689f3556e2342ccdcb3dc91650e68d090565
constant94-dev <constant94@naver.com>
2023-11-25 12:19:19 +0900

    feat(constant): 예외, 주문, 주문 결과, 출력 관련 열거형 상수 작성

--- src/main/java/kr/co/_29cm/homework/constant/view/OutputViewConstant.java
+++ src/main/java/kr/co/_29cm/homework/constant/view/OutputViewConstant.java
@@ -3,14 +3,12 @@ package kr.co._29cm.homework.constant.view;
 public enum OutputViewConstant {
     PRODUCT_SEPARATOR("---------------------------------\n"),
     PRODUCT_ORDER_RESULT("주문 내역:\n"),
-    PRODUCT_ORDER_AMOUNT("주문금액: %d원\n"),
-    PRODUCT_DELIVERY("배송비: %d원\n"),
-    PRODUCT_PAY_AMOUNT("지불금액: %d원\n"),
+    PRODUCT_ORDER_AMOUNT("주문금액: %s원\n"),
+    PRODUCT_DELIVERY_AMOUNT("배송비: %s원\n"),
+    PRODUCT_PAY_AMOUNT("지불금액: %s원\n"),
     PRODUCT_ORDER_THANK("고객님의 주문 감사합니다.\n"),
-    PRODUCT_ORDER_EMPTY("주문 내역이 없습니다."),
-    PRODUCT_AMOUNT_EMPTY("0"),
-    PRODUCT_DELIVERY_EMPTY(""),
-    PRODUCT_PAY_AMOUNT_EMPTY("0");
+    PRODUCT_ORDER_EMPTY("주문 내역이 없습니다.\n"),
+    PRODUCT_DELIVERY_EMPTY("");
 
     private String message;
 
